f1f040c7d1b31590b95c36e3fd4731529c9d3264
Aditya123666 <adityanugrahaadha9@gmail.com>
2019-01-03 22:49:31 +0700

    add-ss

--- app/src/main/java/ormas/aditya/com/ormascibiru/DetailFragment.java
+++ app/src/main/java/ormas/aditya/com/ormascibiru/DetailFragment.java
@@ -1,12 +1,16 @@
 package ormas.aditya.com.ormascibiru;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -14,9 +18,20 @@ import android.widget.TextView;
  */
 public class DetailFragment extends Fragment {
     TextView name, address, type;
+    //aditya
+    Button btn_navigasi;
 
+    String goolgeMap = "com.google.android.apps.maps"; // identitas package aplikasi google masps android
+    Uri gmmIntentUri;
+    Intent mapIntent;
+    String query;
+
+
+    //aditya
     public DetailFragment() {
         // Required empty public constructor
+
+
     }
 
 
@@ -27,6 +42,9 @@ public class DetailFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_detail, container, false);
 
         //initial Id
+
+        btn_navigasi = view.findViewById(R.id.searchalamat);
+
         name = view.findViewById(R.id.name);
         address = view.findViewById(R.id.address);
         type = view.findViewById(R.id.type);
@@ -36,6 +54,30 @@ public class DetailFragment extends Fragment {
         name.setText(b.getString("name"));
         address.setText(b.getString("address"));
         type.setText(b.getString("type"));
+
+        query = b.getString("address");
+
+        btn_navigasi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Buat Uri dari intent string. Gunakan hasilnya untuk membuat Intent.
+                gmmIntentUri = Uri.parse("google.navigation:q=" + query);
+
+                // Buat Uri dari intent gmmIntentUri. Set action => ACTION_VIEW
+                mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+
+                // Set package Google Maps untuk tujuan aplikasi yang di Intent yaitu google maps
+                mapIntent.setPackage(goolgeMap);
+
+                if (mapIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+                    startActivity(mapIntent);
+                } else {
+                    Toast.makeText(v.getContext(), "Google Maps Belum Terinstal. Install Terlebih dahulu.",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+
+        });
 
         return view;
     }
--- app/src/main/java/ormas/aditya/com/ormascibiru/HomeFragment.java
+++ app/src/main/java/ormas/aditya/com/ormascibiru/HomeFragment.java
@@ -6,13 +6,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
-
+    CarouselView carouselView;
+    int[] Images = {R.drawable.gambar1, R.drawable.gambar2};
 
     public HomeFragment() {
         // Required empty public constructor
@@ -23,7 +28,19 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        carouselView = view.findViewById(R.id.carouselView);
+        carouselView.setPageCount(Images.length);
+        carouselView.setImageListener(imageListener);
+
+        return view;
     }
+
+    ImageListener imageListener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            imageView.setImageResource(Images[position]);
+        }
+    };
 
 }
